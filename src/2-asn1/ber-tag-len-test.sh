cb0444769fc14e33fe1e1a235dbb4cb974883a8a
DannyNiu <31366320+dannyniu@users.noreply.github.com>
2021-02-16 05:09:16 +0800

    removed duplicate comment

--- src/2-asn1/ber-tag-len-test.sh
+++ src/2-asn1/ber-tag-len-test.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# [2021-01-10]:
-# It's noticed that, some user-space qemu emulator has unexpected
-# segfault problems. The sole purpose of this test is to verify that,
-# qemu user-space emulator programs are functioning correctly.
-
 testfunc() {
     $exec
 }
