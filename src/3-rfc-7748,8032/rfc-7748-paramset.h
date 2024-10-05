215da4796c1e6e1a6a1cdbc14ee9b55ec21c96ce
DannyNiu/NJF <dannyniu@hotmail.com>
2024-10-05 14:43:38 +0800

    Correct linkage qualifier for PKC parameter set objects.

--- src/3-rfc-7748,8032/rfc-7748-paramset.h
+++ src/3-rfc-7748,8032/rfc-7748-paramset.h
@@ -6,6 +6,6 @@
 #include "rfc-7748.h"
 #include "../2-ec/curves-Mt.h"
 
-PKC_Algo_Inst_t RFC_7748_X25519, RFC_7748_X448;
+extern PKC_Algo_Inst_t RFC_7748_X25519, RFC_7748_X448;
 
 #endif /* MySuiteA_rfc_7748_paramset_h */
