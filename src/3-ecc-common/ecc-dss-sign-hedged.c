d28db69d7cf7b5d279d3f3206bed17ae1bad9516
DannyNiu/NJF <dannyniu@hotmail.com>
2024-11-08 19:01:53 +0800

    Incremental hedged signing for SEC#2-like prime curves.

--- src/3-ecc-common/ecc-dss-sign-hedged.c
+++ src/3-ecc-common/ecc-dss-sign-hedged.c
@@ -6,32 +6,31 @@
 
 #ifdef DSS_SIGN_HEDGED_SM2
 #include "../3-sm2/sm2sig.h"
-#define IsSM2 (signer == SM2SIG_Sign)
+#define IsSM2 (signer == SM2SIG_Sign || signer == SM2SIG_IncSign_Final)
 #else
 #define IsSM2 false
 #endif /* DSS_SIGN_HEDGED_SM2 */
 
-void *ECC_Hedged_Sign(
-    ECC_Hash_Ctx_Hdr_t *restrict x,
-    hmac_drbg_t *restrict prng,
-    PKSignFunc_t signer,
-    void const *restrict msg, size_t msglen,
+// 168 is the rate of SHAKE-128,
+// 136 is the rate of SHAKE-256,
+// 128 is the block size of SHA-512,
+// 64 is the block size of SHA-256.
+// 640 > 168 * 2 + 64.
+#define SEED_STRLEN 640
+
+bool ECC_Hedged_Sign_Prelude(
+    ECC_Hash_Ctx_Hdr_t *restrict x, uint8_t *str,
+    hmac_drbg_t *restrict hrng, unsigned *t_ptr,
     void const *restrict nonce, size_t nlen)
 {
-    // 168 is the rate of SHAKE-128,
-    // 136 is the rate of SHAKE-256,
-    // 128 is the block size of SHA-512,
-    // 64 is the block size of SHA-256.
-    // 640 > 168 * 2 + 64.
-    uint8_t str[640] = {0};
-    unsigned i = 0, t = 0;
+    unsigned i = 0, t = *t_ptr;
 
     void *restrict hctx = DeltaTo(x, offset_hashctx);
     hash_funcs_set_t *hfnx = &x->hfuncs;
 
     vlong_t *vl;
 
-    assert( ((x->curve->plen + 255) & ~255) * 2 + x->hlen < sizeof(str) );
+    assert( ((x->curve->plen + 255) & ~255) * 2 + x->hlen < SEED_STRLEN );
 
     // Z.
 
@@ -56,12 +55,12 @@ void *ECC_Hedged_Sign(
 
     // 000...
 
-    if( prng->prf_blklen > 1 )
+    if( hrng->prf_blklen > 1 )
     {
-        t += prng->prf_outlen + 1; // V and 1 pad byte.
-        t += prng->prf_blklen - 1; // 00h x (blklen - 1).
-        t -= t % prng->prf_blklen; // mod arith.
-        t -= prng->prf_outlen + 1; // V and 1 pad byte.
+        t += hrng->prf_outlen + 1; // V and 1 pad byte.
+        t += hrng->prf_blklen - 1; // 00h x (blklen - 1).
+        t -= t % hrng->prf_blklen; // mod arith.
+        t -= hrng->prf_outlen + 1; // V and 1 pad byte.
     }
     else t += 1;
 
@@ -73,16 +72,38 @@ void *ECC_Hedged_Sign(
 
     // 000...
 
-    if( prng->prf_blklen > 1 )
+    if( hrng->prf_blklen > 1 )
     {
-        t += prng->prf_blklen - 1; // 00h x (blklen - 1).
-        t -= t % prng->prf_blklen; // mod arith.
+        t += hrng->prf_blklen - 1; // 00h x (blklen - 1).
+        t -= t % hrng->prf_blklen; // mod arith.
     }
     else t += 1;
 
     // bits2octets(h1).
 
     hfnx->initfunc(hctx);
+
+    // Leaving Prelude.
+
+    *t_ptr = t;
+    return true;
+}
+
+void *ECC_Hedged_Sign(
+    ECC_Hash_Ctx_Hdr_t *restrict x,
+    hmac_drbg_t *restrict hrng,
+    PKSignFunc_t signer,
+    void const *restrict msg, size_t msglen,
+    void const *restrict nonce, size_t nlen)
+{
+    uint8_t str[SEED_STRLEN] = {0};
+    unsigned t = 0;
+
+    void *restrict hctx = DeltaTo(x, offset_hashctx);
+    hash_funcs_set_t *hfnx = &x->hfuncs;
+
+    if( !ECC_Hedged_Sign_Prelude(x, str, hrng, &t, nonce, nlen) )
+        return NULL;
 
     if( IsSM2 )
     {
@@ -101,6 +122,44 @@ void *ECC_Hedged_Sign(
 
     // Seeding & Signing.
 
-    HMAC_DRBG_Seed(prng, str, t);
-    return signer(x, msg, msglen, (GenFunc_t)HMAC_DRBG_Generate, prng);
+    HMAC_DRBG_Seed(hrng, str, t);
+    return signer(x, msg, msglen, (GenFunc_t)HMAC_DRBG_Generate, hrng);
+}
+
+void *ECC_Hedged_IncSign_Final(
+    ECC_Hash_Ctx_Hdr_t *restrict x,
+    hmac_drbg_t *restrict hrng,
+    PKIncSignFinalFunc_t signer,
+    void const *restrict nonce, size_t nlen)
+{
+    // 168 is the rate of SHAKE-128,
+    // 136 is the rate of SHAKE-256,
+    // 128 is the block size of SHA-512,
+    // 64 is the block size of SHA-256.
+    // 640 > 168 * 2 + 64.
+    uint8_t str[SEED_STRLEN] = {0};
+    unsigned t = 0;
+
+    void *restrict hctx = DeltaTo(x, offset_hashctx);
+    hash_funcs_set_t *hfnx = &x->hfuncs;
+
+    if( !ECC_Hedged_Sign_Prelude(x, str, hrng, &t, nonce, nlen) )
+        return NULL;
+
+    if( IsSM2 )
+    {
+        x->status = 3;
+    }
+    else x->status = 2;
+
+    if( hfnx->xfinalfunc )
+        hfnx->xfinalfunc(hctx);
+
+    hfnx->hfinalfunc(hctx, str+t, x->hlen);
+    t += x->hlen;
+
+    // Seeding & Signing.
+
+    HMAC_DRBG_Seed(hrng, str, t);
+    return signer(x, (GenFunc_t)HMAC_DRBG_Generate, hrng);
 }
--- src/3-ecc-common/ecc-dss-sign-hedged.h
+++ src/3-ecc-common/ecc-dss-sign-hedged.h
@@ -11,12 +11,33 @@
 // https://datatracker.ietf.org/doc/draft-irtf-cfrg-det-sigs-with-noise/
 // Being a draft, technical details are subject to change,
 // and this feature is therefore UNTESTED AND EXPERIMENTAL.
+//
+// 2024-11-08:
+// These signer drivers are intended *only* for passing validation tests.
+// Providing PRNG parameter sets and associating them ECDSA instances had
+// been considered, and dropped - for one, some hash support keying natively,
+// thus making association structure complex; not to mention the current
+// KMAC-DRBG is a transitional solution, as a PRNG built natively from
+// permutations are preferred.
+// The 2nd parameter was renamed to 'HRNG', where 'H' stands for
+// hedged hashing. To differentiate the PRNG passed down to the signer
+// and the RNG used that generates additional randomness, the additional
+// randomness is designed to be passed from an explicit buffer parameter.
+// An update had been introduced here to support the newly added
+// incremental signing feature, even though these are all *only* for
+// passing the validation tests.
 
 void *ECC_Hedged_Sign(
     ECC_Hash_Ctx_Hdr_t *restrict x,
-    hmac_drbg_t *restrict prng,
+    hmac_drbg_t *restrict hrng,
     PKSignFunc_t signer,
     void const *restrict msg, size_t msglen,
+    void const *restrict nonce, size_t nlen);
+
+void *ECC_Hedged_IncSign_Final(
+    ECC_Hash_Ctx_Hdr_t *restrict x,
+    hmac_drbg_t *restrict hrng,
+    PKIncSignFinalFunc_t signer,
     void const *restrict nonce, size_t nlen);
 
 #endif /* MySuiteA_ecc_dss_sign_hedged_h */
